f8a260f5d6c5a6ff703bcd99a08d77f4e857a71c
ypatel-crest <67970063+ypatel-crest@users.noreply.github.com>
2022-06-02 13:28:35 +0530

    Add files via upload

---
+++ versions.tf
@@ -0,0 +1,23 @@
+terraform {
+ cloud {
+    organization = "splunkpcf"
+
+    workspaces {
+      name = "splunkpcf"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.65"
+   }
+    random = {
+      source = "hashicorp/random"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
+  required_version = "~> 1.0"
+}
