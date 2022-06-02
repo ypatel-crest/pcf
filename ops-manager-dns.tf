09c8dcf3d620ee5163e4dfadfed413149188eca8
yash <ypatel@splunk.com>
2022-06-02 13:40:53 +0530

    pcf

--- ops-manager-dns.tf
+++ ops-manager-dns.tf
@@ -2,6 +2,7 @@ data "aws_route53_zone" "hosted" {
   name = var.hosted_zone
 }
 
+
 resource "aws_route53_record" "ops-manager" {
   name = "opsmanager.${var.environment_name}.${data.aws_route53_zone.hosted.name}"
 
